f12a3c2c6ec4a481945e965c9647518dcb5234c2
Jan Skála <j.skala@hotmail.com>
2023-12-06 17:33:12 +0100

    Introduce optimized map

--- 05/Program.cs
+++ 05/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System.Text;
 
-Solve1("sample.txt");
+Solve1("sample.txt"); // 35
 Solve1("input.txt"); // 214922730
-Solve2("sample.txt");
+//Solve2("sample.txt"); // 46
 Solve2("input.txt");
 return;
 
 void Solve1(string path)
 {
     Console.WriteLine(path);
-
     var lines = File.ReadAllLines(path);
     var seeds = ParseSeeds(lines);
     var seedToSoil = ParseMap(lines, "seed-to-soil");
@@ -39,11 +38,7 @@ void Solve1(string path)
 void Solve2(string path)
 {
     Console.WriteLine(path);
-
     var lines = File.ReadAllLines(path);
-    var seeds = ParseSeeds(lines);
-    var chunks = seeds.Chunk(2);
-    
     var seedToSoil = ParseMap(lines, "seed-to-soil");
     var soilToFertilizer = ParseMap(lines, "soil-to-fertilizer");
     var fertilizerToWater = ParseMap(lines, "fertilizer-to-water");
@@ -51,13 +46,12 @@ void Solve2(string path)
     var lightToTemperature = ParseMap(lines, "light-to-temperature");
     var temperatureToHumidity = ParseMap(lines, "temperature-to-humidity");
     var humidityToLocation = ParseMap(lines, "humidity-to-location");
-    
+
     var locations = new List<long>();
-    foreach (var chunk in chunks)
+    var ranges = ParseSeedRanges(lines).OrderBy(x => x.Start).ToList();
+    foreach (var range in ranges)
     {
-        var start = chunk.First();
-        var length = chunk.Last();
-        for (var seed = start; seed < start + length; seed++)
+        foreach (var seed in range.GetSeeds())
         {
             var soil = seedToSoil.Get(seed);
             var fertilizer = soilToFertilizer.Get(soil);
@@ -68,8 +62,14 @@ void Solve2(string path)
             var location = humidityToLocation.Get(humidity);
             locations.Add(location);
         }
+        
+        if (path.Contains("input"))
+        {
+            var index = ranges.IndexOf(range);
+            Console.WriteLine($"Progress: {index + 1}/{ranges.Count}");
+        }
     }
-
+    
     Console.WriteLine("Result: " + locations.Min());
 }
 
@@ -83,7 +83,19 @@ long[] ParseSeeds(string[] lines)
         .ToArray();
 }
 
-Map ParseMap(string[] lines, string name)
+IEnumerable<SeedRange> ParseSeedRanges(string[] lines)
+{
+    var seeds = ParseSeeds(lines);
+    return seeds.Chunk(2)
+        .Select(chunk =>
+        {
+            var start = chunk.First();
+            var length = chunk.Last();
+            return new SeedRange(start, length);
+        });
+}
+
+OptimizedMap ParseMap(string[] lines, string name)
 {
     lines.Single(x => x.Contains(name)); // assertion
     var index = Array.FindIndex(lines, x => x.Contains(name));
@@ -101,7 +113,18 @@ Map ParseMap(string[] lines, string name)
     }
 
     var map = new Map(name, ranges);
-    return map;
+    return map.Optimize();
+}
+
+public record SeedRange(long Start, long Length)
+{
+    public IEnumerable<long> GetSeeds()
+    {
+        for (var i = Start; i < Start + Length; i++)
+        {
+            yield return i;
+        }
+    }
 }
 
 public record Range(long DestinationRangeStart, long SourceRangeStart, long RangeLength)
@@ -146,5 +169,39 @@ public record Map(string Name, IReadOnlyList<Range> Ranges)
             builder.AppendLine(range.ToString());
         }
         return builder.ToString();
+    }
+    
+    public OptimizedMap Optimize() => new(this);
+}
+
+/// <summary>
+/// This optimized map assumes that the keys will be looked up in order.
+/// So caching the last range will speed up the lookup.
+/// </summary>
+public class OptimizedMap
+{
+    private readonly Map _map;
+    private Range? _currentRange;
+
+    public OptimizedMap(Map map) => 
+        _map = map with {Ranges = map.Ranges.OrderBy(x => x.SourceRangeStart).ToList()};
+
+    public long Get(long source)
+    {
+        var destination = source;
+        // range not cached or not found in cached range
+        if (_currentRange is null || !_currentRange.TryGet(source, out destination))
+        {
+            // find the range that contains the source
+            _currentRange = _map.Ranges.FirstOrDefault(x => x.TryGet(source, out destination));
+        }
+        
+        // if no range contains the source, return the source
+        if (_currentRange is null)
+        {
+            return source;
+        }
+        
+        return destination;
     }
 }
